5e83c3eb2de2738fa1c50fc7ec2fd04803ad40fc
Guilherme <guilherme.below@dayback.com.br>
2020-12-17 08:53:14 -0300

    hexgrid working

--- resources/assets/scripts/components/App.js
+++ resources/assets/scripts/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from 'react-hexgrid';
+import { HexGrid, Layout, Hexagon, GridGenerator } from 'react-hexgrid';
 
 export default class App extends Component {
     constructor(props){
@@ -8,33 +8,18 @@ export default class App extends Component {
     }
 
     render(){
-        return(
-            <div >
-              <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
-                {/* Grid with manually inserted hexagons */}
-                <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
-                  <Hexagon q={0} r={0} s={0} />
-                  {/* Using pattern (defined below) to fill the hexagon */}
-                  <Hexagon q={0} r={-1} s={1} fill="pat-1" />
-                  <Hexagon q={0} r={1} s={-1} />
-                  <Hexagon q={1} r={-1} s={0}>
-                    <Text>1, -1, 0</Text>
-                  </Hexagon>
-                  <Hexagon q={1} r={0} s={-1}>
-                    <Text>1, 0, -1</Text>
-                  </Hexagon>
-                  {/* Pattern and text */}
-                  <Hexagon q={-1} r={1} s={0} fill="pat-2">
-                    <Text>-1, 1, 0</Text>
-                  </Hexagon>
-                  <Hexagon q={-1} r={0} s={1} />
-                  <Hexagon q={-2} r={0} s={1} />
-                  <Path start={new Hex(0, 0, 0)} end={new Hex(-2, 0, 1)} />
-                </Layout>
-                <Pattern id="pat-1" link="http://cat-picture" />
-                <Pattern id="pat-2" link="http://cat-picture2" />
-              </HexGrid>
-            </div>
-        )
+
+        const hexagons = GridGenerator.orientedRectangle(20, 10);
+
+        return (
+          <div >
+            <h1>Basic example of HexGrid usage.</h1>
+            <HexGrid width={'100%'} height={'50vh'} viewBox={"0 -5 100 100"}>
+              <Layout size={{ x: 5, y: 5 }}>
+                { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+              </Layout>
+            </HexGrid>
+          </div>
+        );
     }
 }
